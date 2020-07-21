76569a6c52e40a748edfb46700b81929b19acbb7
Akshay Niranjan <anstmfriend@gmail.com>
2020-07-21 09:13:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,6 @@ function destructivelyAddElementToEndOfArray(arr,add){
 function accessElementInArray(arr,ind){
   return arr[ind]
 }
+function destructivelyRemoveElementFromBeginningOfArray(arr){
+  arr.shift()
+}
